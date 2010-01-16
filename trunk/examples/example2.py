ec6958e03b6b2ca3104dbf652be1f21ec8484e97
ivann.exe <ivann.exe@13c15c20-ff47-11de-b4af-25c34a55dae2>
2010-01-16 05:55:05 +0000

    * add example2 * connector: функция req вынесена за пределы класса Connector

--- trunk/examples/example2.py
+++ trunk/examples/example2.py
@@ -1,13 +1,19 @@
 import apivk.secure as secure
 from apivk.vkqueue import VKQueue
 
-q = VKQueue(api_id='1', api_secret='apisecuresecret', req_per_second = 5)
-q.put(secure.saveAppStatus(uid='1', status='Hi, Pavel!'),
-          on_success,
-          on_error)
-
 def on_success(obj):
     print 'on_success ', obj
 
 def on_error(error_code, error_msg, request_params):
     print 'error  ', error_code, ': ', error_msg
+
+api_id = '1788474'
+api_secret = 'apisecuresecret'
+uid = '1857932'
+
+q = VKQueue(api_id, api_secret, 5)
+
+for x in xrange(100):
+    print x
+    q.put(secure.saveAppStatus(uid, x), on_success, on_error)
+
--- trunk/src/apivk/connector.py
+++ trunk/src/apivk/connector.py
@@ -11,27 +11,11 @@ class Connector():
         self.api_secret = api_secret
         self.random = 0
 
-    def req(self, params):
-        """sync request and return object"""
-        #random
+    def _random(self):
         while(True):
             r = random.randint(0, 1000000)
             if (r != self.random):
                 break
-        #params
-        p = {'api_id': self.api_id,
-             'format': 'JSON',
-             'timestamp': unixtime.timestamp(),
-             'random': self.random,
-             'v': api_version}
-        p.update(params)
-        p.update(sig=self._sig(p))
-        data = urllib.urlencode(p)
-        #request
-        response = urllib.urlopen('http://api.vkontakte.ru/api.php', data)
-        str = response.read()
-        #json
-        return json.loads(str)
 
     def _sig(self, params):
         m = hashlib.md5()
@@ -41,3 +25,23 @@ class Connector():
         m.update(''.join(str))
         m.update(self.api_secret)
         return m.hexdigest()
+
+    def params(self, method_params):
+        self._random()
+        p = {'api_id': self.api_id,
+             'format': 'JSON',
+             'timestamp': unixtime.timestamp(),
+             'random': self.random,
+             'v': api_version}
+        p.update(method_params)
+        p.update(sig=self._sig(p))
+        return p
+
+
+def req(method_params, conn):
+    """sync request and return object"""
+    p = conn.params(method_params)
+    data = urllib.urlencode(p)
+    response = urllib.urlopen('http://api.vkontakte.ru/api.php', data)
+    str = response.read()
+    return json.loads(str)

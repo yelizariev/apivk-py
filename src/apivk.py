ee52ae9d4042cdf1591130125dfb24a9b90d69c6
ivann.exe <ivann.exe@13c15c20-ff47-11de-b4af-25c34a55dae2>
2010-02-12 13:42:57 +0000

    * add VKAuth

--- src/apivk.py
+++ src/apivk.py
@@ -1,4 +1,4 @@
-__all__=["VKQueue", "VKError", "vkparse", "VKReq"]
+__all__=["VKQueue", "VKError", "VKAuth"]
 
 #####################
 ### common imports
@@ -27,8 +27,21 @@ def vktime():
 DELTA_UNIXTIME = vktime() - int(time.time())
 def timestamp():
     return int(time.time())+DELTA_UNIXTIME
+###########
+### VKAuth
+class VKAuth:
+    def __init__(self, api_id, api_secret):
+        self.api_id = api_id
+        self.api_secret = api_secret
 
-
+    def check(self, id, auth_key):
+        m = hashlib.md5()
+        m.update(self.api_id)
+        m.update('_')
+        m.update(id)
+        m.update('_')
+        m.update(self.api_secret)
+        return m.hexdigest()==auth_key
 
 ############
 ### vkqueue
